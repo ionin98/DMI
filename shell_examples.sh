e21b469f95e7c9a1786670ddcbfb745f88e27039
Guest <ionin2@inbox.lv>
2017-09-28 09:51:53 +0300

    20170928_09_50

---
+++ shell_examples.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#4. piemērs
+echo "Atbilde uz jautajumiem-kas sobrid tiek pildits?" $0
+echo $n
+echo "Skriptam nodoto argumentu skaits: "$#
+
+#3. piemērs
+#NAME="Vards Uzvārds"
+#echo $NAME
+#unset NAME
+#echo $NAME
+
+#1. piemērs
+#NAME="Vards Uzvārds"
+#readonly NAME
+#echo $NAME
+#NAME="Uzvārds Vards"
+#echo $NAME
+
+
+
+#1. piemērs
+#NAME="Vards Uzvārds"
+#echo $NAME
+
