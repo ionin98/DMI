4fc35e605eb0f94d67c03be5d8283895cc52e809
Guest <ionin2@inbox.lv>
2017-11-02 11:52:21 +0200

    20171102_11_50

---
+++ PYTHON/hello.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# https://www.tutorialspoint.com/unix_commands/echo.htm
+# Example 3. -> 6.
+# echo -e '...'[SHELL] -> print "..." [PYTHON]
+'''
+print "Hello World!"
+'''
+'''
+# Example 3
+print "Here\bthe \bspaces \bare \bbackspaced."
+'''
+'''
+# Example 4
+print  "Here \nthe \nspaces \nare \nnewlined."
+'''
+'''
+# Example 5
+print "Here \tthe \tspaces \thave \thorizontal \ttab \tspaces."
+'''
+# Example 6
+print  "Here \vthe \vspaces \vhave \vvertical \vtab \vspaces."
+
+print  "Here \n \nspaces \nhave \nvertical \ntab \nspaces."
